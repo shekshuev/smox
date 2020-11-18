15dd837b2a9b4918e51634f45c402a705ecffcd5
Сергей Шекшуев <danger_x@MacBook-Pro-Sergej.local>
2020-11-18 23:39:48 +0300

    added sqlalchemy

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,8 +3,9 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from database.social.models import *
-from database import dbhandle 
+from app.config import Config
 from app.api import api as app_api
 from auth.api import api as auth_api
 from app.views import views as app_views
@@ -15,18 +16,10 @@ app.register_blueprint(app_api)
 app.register_blueprint(auth_api)
 app.register_blueprint(app_views)
 app.register_blueprint(auth_views)
-app.config["JWT_SECRET_KEY"] = "Fuck them all!"
+app.config.from_object(Config)
 jwt = JWTManager(app)
 CORS(app)
 
-usersdb = SQLAlchemy(app)
-
-dbhandle.connect()
-AccessProfileModel.create_table()
-LogModel.create_table()
-SourceModel.create_table()
-TaskModel.create_table()
-TaskSourceModel.create_table()
-PostAttachmentModel.create_table()
-PostTimestampModel.create_table()
-PostModel.create_table()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from database.social.models2 import *
---
+++ src/app/config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "Fuck them all!"
